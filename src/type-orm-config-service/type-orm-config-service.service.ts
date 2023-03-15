70cd1696c0b34cfc23981823981944c93931b177
Gia <gia@roundsafely.com>
2023-03-15 14:35:04 +0700

    ConfigModule setup

---
+++ src/type-orm-config-service/type-orm-config-service.service.ts
@@ -0,0 +1,28 @@
+import { Category } from '@/entities/category.entity';
+import { Label } from '@/entities/label.entity';
+import { Task } from '@/entities/task.entity';
+import { User } from '@/entities/user.entity';
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+@Injectable()
+export class TypeOrmConfigServiceService implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+  createTypeOrmOptions(
+    connectionName?: string,
+  ): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
+    return {
+      type: 'postgres',
+      host: this.configService.get<string>('database.host'),
+      port: this.configService.get<number>('database.port'),
+      username: this.configService.get<string>('database.username'),
+      password: this.configService.get<string>('database.password'),
+      database: this.configService.get<string>('database.database'),
+      schema: this.configService.get<string>('database.schema'),
+      synchronize: true,
+      logging: true,
+      subscribers: [],
+      entities: [User, Label, Task, Category],
+    };
+  }
+}
