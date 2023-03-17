ee1897cd8767a0312702af2fc3eb1b03270cf20f
Hoang Cong Thanh Nhan <nhan.hoangcongthanh@ncc.asia>
2023-03-17 23:31:41 +0700

    21 - CRUD for category

--- src/features/category/category.service.ts
+++ src/features/category/category.service.ts
@@ -1,15 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Category } from '@/entities/category.entity';
+import { User } from '@/entities/user.entity';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FindManyOptions, Repository } from 'typeorm';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>,
+  ) {}
+
+  async create(createCategoryDto: CreateCategoryDto, currentUser: User) {
+    const existingCategory = await this.categoryRepository.findOne({
+      where: { name: createCategoryDto.name, userId: currentUser.id },
+    });
+
+    if (existingCategory) {
+      throw new ConflictException('Category already exists');
+    }
+
+    const category = new Category();
+    category.name = createCategoryDto.name;
+    category.user = currentUser;
+
+    return this.categoryRepository.save(category);
   }
 
-  findAll() {
-    return `This action returns all category`;
+  findAll(filter: FindManyOptions) {
+    return this.categoryRepository.find(filter);
   }
 
   findOne(id: number) {
@@ -17,10 +38,10 @@ export class CategoryService {
   }
 
   update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+    return this.categoryRepository.update(id, updateCategoryDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} category`;
+    return this.categoryRepository.delete(id);
   }
 }
